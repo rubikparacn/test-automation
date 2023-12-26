31373030c99ff87a90024104243f2524b65be27e
onur.gokay <onur.gokay@rubikpara.com>
2023-12-26 17:18:28 +0300

    ##

--- cypress/e2e/project/merchant/payment/link-3d-enable.cy.js
+++ cypress/e2e/project/merchant/payment/link-3d-enable.cy.js
@@ -83,7 +83,7 @@ describe('Create payment with link', () => {
 
     })
 
-    it('Set calback url', () => {
+    it('Set callback url', () => {
         cy.get('#input-url').type("https://testmerchant.rubikpara.com/link-payment")
         cy.wait(1000);
 
